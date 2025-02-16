89f1e99f2af179ed4fddca89b2af599f3345acc7
PauliusJaskutelis <pjaskutelis@gmail.com>
2025-02-16 11:32:44 +0200

    Created connection to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.warning("Failed to Connect to mongoDB: ", err));
 
 const app = express();
 app.use(express.json());
@@ -8,5 +15,4 @@ app.use(cors());
 
 app.get("/", (req, res) => res.send("API Running..."));
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
